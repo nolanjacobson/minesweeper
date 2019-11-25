a826480a75bfcd10d060d040a4bbf8f33dc8fa95
Nolan Jacobson <theprophet0@users.noreply.github.com>
2019-11-25 16:18:14 -0500

    after diff update

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Cells from './Cell'
 import Screen from './Screen'
@@ -14,67 +14,63 @@ import soundFiles3 from '../sounds/soundOnRightClick.mp3'
 // add sound effects
 // create drop down buttons
 // create a modal screen
-// how's the relationship between parent and child determined? 
+// how's the relationship between parent and child determined?
 
 const MainGame = () => {
-
   const [table, getTable] = useState([])
   const [gameID, getGameID] = useState([])
   const [mines, getMines] = useState([])
   const [state, getState] = useState([])
-  // const [easyDif, getDifficultyEasy] = useState(0)
-  // const [mediumDif, getDifficultyMedium] = useState(1)
-  // const [hardDif, getDifficultyHard] = useState(2)
-
-  const createTheTable = async () => {
-    const response = await axios.post('https://minesweeper-api.herokuapp.com/games', {difficulty: 0})
-    getGameID(response.data.id)
-    getTable(response.data.board)
-    getMines(response.data.mines)
-    getState(response.data.state)
-  }
 
   useEffect(() => {
-    createTheTable()
+    createGame(0)
   }, [])
 
   const rightClick = async (x, y) => {
-    const response = await axios.post(`https://minesweeper-api.herokuapp.com/games/${gameID}/flag`, 
-    {
-    row: x,
-    col: y})
+    const response = await axios.post(
+      `https://minesweeper-api.herokuapp.com/games/${gameID}/flag`,
+      {
+        row: x,
+        col: y
+      }
+    )
     getTable(response.data.board)
     getMines(response.data.mines)
     getState(response.data.state)
   }
 
   const leftClick = async (x, y) => {
-    const response = await axios.post(`https://minesweeper-api.herokuapp.com/games/${gameID}/check`, {
-      row: x,
-      col: y
-    })
+    const response = await axios.post(
+      `https://minesweeper-api.herokuapp.com/games/${gameID}/check`,
+      {
+        row: x,
+        col: y
+      }
+    )
     getTable(response.data.board)
     getMines(response.data.mines)
     getState(response.data.state)
     console.log(state)
   }
 
-  const easy = async () => {
-    const response = await axios.post('https://minesweeper-api.herokuapp.com/games', {difficulty: 0})
-    getGameID(response.data.id)
+  const clicked = (action, x, y) =>{
+    const response = await axios.post(
+      `https://minesweeper-api.herokuapp.com/games/${gameID}/${action}`,
+      {
+        row: x,
+        col: y
+      }
+    )
     getTable(response.data.board)
     getMines(response.data.mines)
     getState(response.data.state)
   }
-  const medium = async () => {
-    const response = await axios.post('https://minesweeper-api.herokuapp.com/games', {difficulty: 1})
-    getGameID(response.data.id)
-    getTable(response.data.board)
-    getMines(response.data.mines)
-    getState(response.data.state)
-  }
-  const hard = async () => {
-    const response = await axios.post('https://minesweeper-api.herokuapp.com/games', {difficulty: 2})
+
+  const createGame = async diff => {
+    const response = await axios.post(
+      'https://minesweeper-api.herokuapp.com/games',
+      { difficulty: diff }
+    )
     getGameID(response.data.id)
     getTable(response.data.board)
     getMines(response.data.mines)
@@ -90,81 +86,73 @@ const MainGame = () => {
   //     // getState(0)
   //     createTheTable()
   //   }
-    // how can I use a return statement here passing a parameter?
+  // how can I use a return statement here passing a parameter?
   // }
 
   return (
     <>
-    <Sound 
-    url={soundFiles}
-    autoLoad = {true}
-    playStatus={Sound.status.PLAYING}/>
-    <Header 
-    />
-    <Buttons 
-    createTheTable = {createTheTable}
-    difficulty = {easy}
-    difficulty1 = {medium}
-    difficulty2  = {hard}/>
+      <Sound
+        url={soundFiles}
+        autoLoad={true}
+        playStatus={Sound.status.PLAYING}
+      />
+      <Header />
+      <Buttons
+        createTheTable={() => createGame(0))}
+        difficulty={() => createGame(0)}
+        difficulty1={() => createGame(1)}
+        difficulty2={() => createGame(2)}
+      />
 
-    <section className="flexBox">
-    <table>
-      <tbody>
-        {table.map((col, x) => {
-          return (
-            <tr key ={[x]}>
-          {col.map((row, y) => {
-            // force reset if not playing
-            if (state == 'new' || state == 'playing') {
-            return (
-             <Cells 
-              key={[y]}
-              display={table[x][y]}
-              // what functions are being run here and why are there no parameters or name?
-              leftClick= {() => leftClick(x,y)}
-              rightClick = {() => rightClick(x, y)}
-             />
-          
+      <section className="flexBox">
+        <table>
+          <tbody>
+            {table.map((col, x) => {
+              return (
+                <tr key={[x]}>
+                  {col.map((row, y) => {
+                    // force reset if not playing
+                    if (state == 'new' || state == 'playing') {
+                      return (
+                        <Cells
+                          key={[y]}
+                          display={table[x][y]}
+                          leftClick={() => clicked("check",x, y)}
+                          rightClick={() => clicked("flag",x, y)}
+                        />
+                      )
+                    } else {
+                      if (state === 'lost') {
+                        return (
+                          <>
+                            <Cells key={[y]} display={table[x][y]} />
+                            <Screen
+                              result="You Lost"
+                              restartGame={createTheTable}
+                            />
+                          </>
+                        )
+                      } else if (state === 'won') {
+                        return (
+                          <>
+                            <Cells key={[y]} display={table[x][y]} />
+                            <Screen
+                              result="You Won"
+                              restartGame={createTheTable}
+                            />
+                          </>
+                        )
+                      }
+                    }
+                  })}
+                </tr>
               )
-            }
-          
-              else {
-                if (state === 'lost') {
-                return (
-                  <>
-                  <Cells 
-                  key={[y]}
-                  display={table[x][y]}/>
-                  <Screen 
-                  result = 'You Lost'
-                  restartGame = {createTheTable}
-                  />
-                  </>
-                )
-                }
-                else if (state === 'won') {
-                  return (
-                    <>
-                    <Cells 
-                    key={[y]}
-                    display={table[x][y]}/>
-                    <Screen 
-                    result = 'You Won'
-                    restartGame = {createTheTable}
-                    />
-                    </>
-                  )
-                }
-              }
-          })}
-          </tr>
-        )})}
-        </tbody>
-      </table>
-    </section>
+            })}
+          </tbody>
+        </table>
+      </section>
     </>
   )
 }
 
-export default MainGame;
-
+export default MainGame
